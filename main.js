22a6fd7c01ab5f97f3af70159403154b5c6f7831
Mykyta Holovakin <golovakin.kinds@gmail.com>
2023-05-16 11:35:51 +0300

    test

--- main.js
+++ main.js
@@ -179,7 +179,7 @@ function draw(){
         const rotationZ = m4.axisRotation([0,0,1], alphaRadians);
         const rotationX = m4.axisRotation([1,0,0], -betaRadians);
         const rotationY = m4.axisRotation([0,1,0], gammaRadians);
-        const rotation = m4.multiply(m4.multiply(rotationX, rotationZ), rotationY);
+        const rotation = m4.multiply(m4.multiply(rotationX, rotationY), rotationZ);
         const translation = m4.translation(0, 0, -20);
         modelView = m4.multiply(rotation, translation);
     } else{
@@ -494,9 +494,9 @@ const requestDeviceOrientation = async () => {
       console.log('Permission granted');
       window.removeEventListener('devicemotion', latestHandler, true);
       latestHandler = e => {
-        latestEvent.alpha = Math.atan(e.acceleration.y, e.acceleration.z);
-        latestEvent.beta =  Math.atan(-e.acceleration.x, (e.acceleration.y ** 2 + e.acceleration.z ** 2));
-        latestEvent.gamma = Math.atan(-e.acceleration.y, e.acceleration.x);
+        latestEvent.alpha = Math.atan(e.acceleration.x, e.acceleration.z);
+        latestEvent.beta = Math.atan(e.acceleration.y, (e.acceleration.x ** 2 + e.acceleration.z ** 2));
+        latestEvent.gamma = Math.atan(-e.acceleration.x, e.acceleration.y);
         latestEvent.event = e;
       };
       window.addEventListener('devicemotion', latestHandler, true);
