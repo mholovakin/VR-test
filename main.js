7ab76898e80449e26db2d7d588c75f387aea120a
Mykyta Holovakin <golovakin.kinds@gmail.com>
2023-05-16 11:24:55 +0300

    test

--- main.js
+++ main.js
@@ -494,9 +494,9 @@ const requestDeviceOrientation = async () => {
       console.log('Permission granted');
       window.removeEventListener('devicemotion', latestHandler, true);
       latestHandler = e => {
-        latestEvent.alpha = Math.atan2(-e.acceleration.x, (e.acceleration.y ** 2 + e.acceleration.z ** 2));
-        latestEvent.beta =  Math.atan2(e.acceleration.y, e.acceleration.z);
-        latestEvent.gamma = Math.atan2(-e.acceleration.y, e.acceleration.x);
+        latestEvent.alpha = Math.atan(e.acceleration.y, e.acceleration.z);
+        latestEvent.beta =  Math.atan(-e.acceleration.x, (e.acceleration.y ** 2 + e.acceleration.z ** 2));
+        latestEvent.gamma = Math.atan(-e.acceleration.y, e.acceleration.x);
         latestEvent.event = e;
       };
       window.addEventListener('devicemotion', latestHandler, true);
